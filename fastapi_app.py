e3b33cd57500f84075d9e22d4b747ff757868c8d
Viktor Borisov <borisov_vk@mail.ru>
2024-03-11 23:31:21 +0300

    add load image fastapi

--- fastapi_app.py
+++ fastapi_app.py
@@ -7,25 +7,63 @@ from fastapi import File, UploadFile
 from fastapi import FastAPI
 import logging
 
+DATABASE_PATH = "data/database"
+PATH_TO_CLOTHES = "{}/user_{}/clothes/image_{}/original"
+PATH_TO_FULL_BODY = "{}/user_{}/full_body/image_{}/original"
 
 logging.basicConfig(level=logging.INFO, filename="logs/fastapi_logs.log",filemode="w")
 
 app = FastAPI()
 
+def check_path(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-@app.post("/data/human")
-def upload(user_id: int, image_id: int, file: UploadFile = File(...)):
-    logging.info("Got upload human full growth request")
+
+@app.post("/data/load")
+def upload(image_type:str, user_id: int, image_id: int, file: UploadFile = File(...)):
+    logging.info("Got POST image request")
     try:
         contents = file.file.read()
-        logging.info("human object")
-        path_to_save = os.path.join("data", "from_fast_api", file.filename)
-        with open(path_to_save, 'wb') as f:
+        extension = file.filename.split(".")[-1]
+        save_filename = f'{image_id}.{extension}'
+        
+        if image_type == "full-body":
+            path_to_save = PATH_TO_FULL_BODY.format(DATABASE_PATH, user_id, image_id)
+        elif image_type == "cloth":
+            path_to_save = PATH_TO_CLOTHES.format(DATABASE_PATH, user_id, image_id)
+        else:
+            raise Exception("Incorrect input image_type. Should be one of: 'full-body', 'cloth'")
+
+        check_path(path_to_save)
+        file_path = os.path.join(path_to_save, save_filename)
+
+        with open(file_path, 'wb') as f:
             f.write(contents)
-    except Exception:
-        return {"message": "There was an error uploading the file"}
+    except Exception as e:
+        return {"message": f"There was an error uploading the full-body file: {e}"}
     finally:
         file.file.close()
 
-    return {"message": f"Successfully uploaded {file.filename}"}
+    return {"message": f"Successfully uploaded full-body"}
 
+
+# @app.post("/data/cloth")
+# def upload(user_id: int, image_id: int, file: UploadFile = File(...)):
+#     logging.info("Got cloth POST request")
+#     try:
+#         contents = file.file.read()
+#         extension = file.filename.split(".")[-1]
+#         save_filename = f'{image_id}.{extension}'
+        
+#         path_to_save = PATH_TO_CLOTHES.format(DATABASE_PATH, user_id, image_id)
+#         check_path(path_to_save)
+#         file_path = os.path.join(path_to_save, save_filename)
+#         with open(file_path, 'wb') as f:
+#             f.write(contents)
+#     except Exception as e:
+#         return {"message": f"There was an error uploading the cloth file: {e}"}
+#     finally:
+#         file.file.close()
+
+#     return {"message": f"Successfully uploaded cloth-body"}
--- test_request.py
+++ test_request.py
@@ -2,24 +2,26 @@ import requests
 
 # example of loading image with human
 def human_image_example():
-    url = 'http://localhost:8000/data/human'
+    url = 'http://localhost:8000/data/load'
 
     path_to_image = "data/example/human.jpg"
 
-    payload = {"user_id":0, "image_id":0, }
+    payload = {"user_id":0, "image_id":1, "image_type":"full-body" }
     files = {"file":open(path_to_image, 'rb'), }
     resp = requests.post(url, params=payload,files=files )#auth=("user", "password"))
     print(resp.json())
 
+
 def cloth_image_example():
-    url = 'http://localhost:8000/data/human'
+    url = 'http://localhost:8000/data/load'
 
-    path_to_image = "data/example/t_shirt.jpg"
+    path_to_image = "data/example/t_shirt.png"
 
-    payload = {"user_id":0, "image_id":0, }
+    payload = {"user_id":0, "image_id":1, "image_type":"cloth" }
     files = {"file":open(path_to_image, 'rb'), }
     resp = requests.post(url, params=payload,files=files )#auth=("user", "password"))
     print(resp.json())
 
 if __name__ == '__main__':
     human_image_example()
+    cloth_image_example()
