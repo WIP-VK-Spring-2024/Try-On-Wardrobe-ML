a8eeff5b8379f8b5a24a8a0c54ce7552b3c82f5f
Viktor Borisov <borisov_vk@mail.ru>
2024-03-15 11:00:14 +0300

    cuda docker first commit

--- ml/processing/tags.py
+++ ml/processing/tags.py
@@ -1,9 +1,19 @@
 import transformers
 
+from transformers import CLIPProcessor, CLIPModel
 
-def get_tags(user_id, image_id, tags,):
-    """
-    Selects tags from given tags through image
-    with image_id and user_id
-    """
-    pass
+
+class Tagger:
+    def __init__(self, dataset_path, weights_path, device='cpu'):
+        self.dataset_path = dataset_path
+        self.clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+        self.clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+
+    def get_tags(self, user_id, image_id, tags,):
+        """
+        Selects tags from given tags through image
+        with image_id and user_id
+        """
+        inputs = self.clip_processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True)
+        outputs = self.clip_model(**inputs)
+
---
+++ ml/tmp.py
@@ -0,0 +1,4 @@
+import torch
+#from processing.tags import Tagger
+#import torch
+#print(torch.cuda.is_available())
