354b04feb7aaa036e0f444c14b42ba62a79ce2a3
timofeytst <yarik.mist@gmail.com>
2024-03-23 21:35:33 +0300

    [+] Added get from amazon s3.

--- app/internal/routes/amazon_s3.py
+++ app/internal/routes/amazon_s3.py
@@ -1,8 +1,10 @@
 """Amazon S3 router."""
+from typing import Optional
 
 from fastapi import File, UploadFile, Depends, status, APIRouter
+from fastapi.responses import StreamingResponse
 
-from app.internal.services.amazon_s3 import AmazonS3Service
+from app.internal.services import AmazonS3Service
 from app.pkg.models import ResponseMessage
 
 aws_router = APIRouter(
@@ -19,10 +21,11 @@ aws_router = APIRouter(
 )
 async def upload_file(
     file: UploadFile = File(...),
+    folder: Optional[str] = None,
     aws_service: AmazonS3Service = Depends(AmazonS3Service),
 ):
-    aws_service.upload(file)
-    return ResponseMessage(message="File successfully uploaded.")
+    aws_service.upload(file=file.file, file_name=file.filename, folder=folder)
+    return ResponseMessage(message=f"File {folder}/{file.filename} successfully uploaded.")
 
 @aws_router.delete(
     "/delete/{file_name}",
@@ -32,7 +35,26 @@ async def upload_file(
 )
 async def delete_file(
     file_name: str,
+    folder: Optional[str] = None,
     aws_service: AmazonS3Service = Depends(AmazonS3Service),
 ):
-    aws_service.delete_file(file_name=file_name)
-    return ResponseMessage(message=f"File {file_name} deleted successfully")
+    aws_service.delete(file_name=file_name, folder=folder)
+    return ResponseMessage(message=f"File {folder}/{file_name} deleted successfully")
+
+
+@aws_router.get(
+    "/read/{file_name}",
+    status_code=status.HTTP_200_OK,
+    description="Get image from AWS.",
+)
+async def read(
+    file_name: str,
+    folder: Optional[str] = None,
+    aws_service: AmazonS3Service = Depends(AmazonS3Service),
+):
+    file_content = aws_service.read(file_name=file_name, folder=folder)
+    return StreamingResponse(
+        file_content, 
+        media_type='application/octet-stream',
+        headers={"Content-Disposition": f"attachment; filename={file_name}"},
+    )
--- app/internal/services/amazon_s3.py
+++ app/internal/services/amazon_s3.py
@@ -1,8 +1,9 @@
 """Amazon S3 service."""
+from typing import BinaryIO
+from io import BytesIO
 
 import boto3
 from botocore.exceptions import ClientError
-from fastapi import FastAPI, File, UploadFile
 
 from app.pkg.models.base.exception import BaseAPIException
 from app.pkg.settings import settings
@@ -23,17 +24,33 @@ class AmazonS3Service:
         )
         self.s3_bucket_name = settings.AWS.BUCKET_NAME
 
+    def get_file_path(self, file_name: str, folder: str = None) -> str:
+        file_path = f"{folder}/{file_name}" if folder else file_name
+        return file_path
 
-    def upload(self, file: UploadFile = File(...)):
+    def upload(self, file: BinaryIO, file_name: str, folder: str = None) -> None:
+        file_path = self.get_file_path(file_name, folder)
+        extra_args = {'ACL': 'public-read'}
         try:
-            res = self.s3.upload_fileobj(file.file, self.s3_bucket_name, file.filename)
+            res = self.s3.upload_fileobj(file, self.s3_bucket_name, file_path, ExtraArgs=extra_args)
             logger.info("Uploaded file to S3, response: %s", res)
         except ClientError as e:
             raise BaseAPIException(message=f"Failed to upload file to S3: {e}")
         
-    def delete_file(self, file_name: str):
+    def delete(self, file_name: str, folder: str = None) -> None:
+        file_path = self.get_file_path(file_name, folder)
         try:
-            res = self.s3.delete_object(Bucket=self.s3_bucket_name, Key=file_name)
+            res = self.s3.delete_object(Bucket=self.s3_bucket_name, Key=file_path)
             logger.info("Deleted file from S3, response: %s", res)
         except ClientError as e:
             raise BaseAPIException(message=f"Failed to delete file from S3: {e}")
+        
+    def read(self, file_name: str, folder: str = None) -> BinaryIO:
+        file_path = self.get_file_path(file_name, folder)
+        try:
+            response = self.s3.get_object(Bucket=self.s3_bucket_name, Key=file_path)
+            file_content = response["Body"].read()
+            buffer = BytesIO(file_content)
+            return buffer
+        except ClientError as e:
+            raise BaseAPIException(message=f"Failed to read file from S3: {e}")
