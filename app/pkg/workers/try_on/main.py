e4dd860f6bc1e5f4176882d11cec1169d1b79c9a
timofeytst <yarik.mist@gmail.com>
2024-03-24 19:25:37 +0300

    [+] Added model pipeline.

--- app/pkg/workers/try_on/main.py
+++ app/pkg/workers/try_on/main.py
@@ -7,6 +7,8 @@ from app.internal.repository.rabbitmq.model_response import ModelRespRepository
 from app.pkg.workers.try_on.worker import TryOnWorker
 from app.internal.services import AmazonS3Service
 from app.pkg.ml.try_on.preprocessing.aggregator import ClothProcessor
+from app.pkg.ml.try_on.preprocessing.aggregator import HumanProcessor
+from app.pkg.ml.try_on.lady_vton import LadyVtonAggregator
 
 __all__ = ["start_worker"]
 
@@ -15,13 +17,18 @@ def start_worker():
     task_repository = ModelTaskRepository()
     resp_repository = ModelRespRepository()
     file_service = AmazonS3Service()
+
     clothes_model = ClothProcessor()
+    human_model = HumanProcessor()
+    try_on_model = LadyVtonAggregator()
 
     model_worker = TryOnWorker(
         task_repository=task_repository,
         resp_repository=resp_repository,
         file_service=file_service,
         clothes_model=clothes_model,
+        human_model=human_model,
+        try_on_model=try_on_model,
     )
     asyncio.run(model_worker.listen_queue())
 
--- app/pkg/workers/try_on/worker.py
+++ app/pkg/workers/try_on/worker.py
@@ -6,6 +6,8 @@ from app.internal.services import AmazonS3Service
 from app.pkg.models import TryOnResponseCmd
 from app.pkg.logger import get_logger
 from app.pkg.ml.try_on.preprocessing.aggregator import ClothProcessor
+from app.pkg.ml.try_on.preprocessing.aggregator import HumanProcessor
+from app.pkg.ml.try_on.lady_vton import LadyVtonAggregator
 
 logger = get_logger(__name__)
 
@@ -15,6 +17,10 @@ class TryOnWorker:
     task_repository: ModelTaskRepository
     resp_repository: ModelRespRepository
     file_service: AmazonS3Service
+    clothes_model: ClothProcessor
+    human_model = HumanProcessor
+    try_on_model = LadyVtonAggregator
+
 
     def __init__(
         self,
@@ -22,35 +28,47 @@ class TryOnWorker:
         resp_repository: ModelRespRepository,
         file_service: AmazonS3Service,
         clothes_model: ClothProcessor,
+        human_model = HumanProcessor,
+        try_on_model = LadyVtonAggregator,
     ):
         self.task_repository = task_repository
         self.resp_repository = resp_repository
         self.file_service = file_service
     
         self.clothes_model = clothes_model
+        self.human_model = human_model
+        self.try_on_model = try_on_model
 
     async def listen_queue(self):
         async for message in self.task_repository.read():
             logger.info("New message [%s]", message)
 
-            user_image, user_image_path = self.file_service.read_and_save(
+            user_image = self.file_service.read(
                 file_name=message.user_image_id,
                 folder=message.user_image_dir,
             )
 
-            clothes_image, clothes_image_path = self.file_service.read_and_save(
+            clothes_image = self.file_service.read(
                 file_name=message.clothes_id,
                 folder=message.clothes_dir,
             )
+
             logger.info(
-                "New message local saved user file: [%s], clothes file: [%s]",
-                user_image_path,
-                clothes_image_path,
+                "Started removing background clothes id: [%s]",
+                message.clothes_id,
             )
-            
+
+            # Model pipeline           
             # Remove model background
             cutted_clothes = self.clothes_model.consistent_forward(clothes_image)
-            # try_on_file_path = await try_on_model.pipeline(try_on_file_path)
+            logger.debug("End remove background, result: [%s].", cutted_clothes["cloth"])
+            processed_user = self.human_model.consistent_forward(user_image)
+            logger.debug("End human processing, result: [%s]", processed_user["parse_orig"])
+            
+            processed_user.update(cutted_clothes)
+            try_on = self.try_on_model(processed_user)
+            logger.debug("End try on, result: [%s]", try_on)
+            # End model pipeline
 
             res_file_name = message.clothes_id
             res_file_dir = f"try_on/{message.user_image_id}" # TODO: add path to settings
@@ -58,7 +76,7 @@ class TryOnWorker:
             res_file_path = f"{res_file_dir}/{res_file_name}"
 
             self.file_service.upload(
-                file=cutted_clothes["cloth"],
+                file=try_on,
                 file_name=res_file_name,
                 folder=res_file_dir,
             )
