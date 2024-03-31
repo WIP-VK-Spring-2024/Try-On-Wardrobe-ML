0eb22bb0770bef1d2ea5acc04644dcbdc4911582
Viktor Borisov <borisov_vk@mail.ru>
2024-03-31 12:38:57 +0000

    i/o autotags correction

--- app/pkg/ml/autotags/autotag.py
+++ app/pkg/ml/autotags/autotag.py
@@ -3,8 +3,11 @@ import io
 
 import torch
 from transformers import AutoProcessor, CLIPModel, AutoTokenizer
+from torch.nn import Softmax
+from PIL import Image
 
 from app.pkg.ml.buffer_converters import BytesConverter
+
 
 class AutoTagger:
     def __init__(self, device="cuda:0"):
@@ -13,9 +16,9 @@ class AutoTagger:
         self.tokenizer =  AutoTokenizer.from_pretrained("openai/clip-vit-base-patch32")
         self.device = device
         self.bytes_converter = BytesConverter()
+        self.soft_max_function = Softmax(dim=1)
 
-
-    def get_tags(self, input_data: Dict[str, Union[io.BytesIO, List[List[str]]]]) -> Dict[str, float]:
+    def forward(self, input_data: Dict[str, Union[io.BytesIO, Dict[str, List[str]]]]) -> Dict[str, Dict[str, float]]:
         """
         Gets probability for each tag
         
@@ -23,33 +26,70 @@ class AutoTagger:
             input_data - Dict[str, io.BytesIO] - dict, contained folowing structure:
                 {
                 "image":io.BytesIO,
-                "tags":List[List[str]]
+                "tags":Dict[str, List[str]]
                 }
         Returns:
             dict with probabilities
         
-        Example for input tags:[["winter", "summer"], ["t-shirt","pants", "shoes"]]
-        Will return {"winter":0.79, "summer":0.21, "t-shirt":0.1, "pants":0.15, "shoes":0.75}
+        Example for input tags:
+        {
+            "seasons": ["summer", "winter", "autumn", "spring"],
+            "types": ["upper", "lower", "dresses", "shoes"]
+        }
+
+        Will return:
+        {
+            "seasons": {
+                "summer": 0.1,
+                "winter": 0.5,
+                "autumn": 0.3,
+                "spring": 0.1
+            },
+            "types": {...}
+        }
+
         Multy list format need for normalizing values into probabilities
         """
         image = self.bytes_converter.bytes_to_image(input_data["image"])
+        tags = input_data['tags']
+        output_dict = self._get_tags(image, tags)
+        return output_dict
+
+    @torch.inference_mode()
+    def _get_tags(self, image:Image, tags:Dict[str, List[str]]) -> Dict[str, float]:
+        """
+        Gets probability for each tag
+        
+        Args:
+            image:Image - image to get tags
+            tags:Dict[str, List[str]]] - tags
+            
+        Returns:
+            dict with probabilities   
+        """
         image_inputs = self.processor(images=image, return_tensors="pt")
         self._input_to_device(image_inputs)
 
-        # image_inputs['pixel_values'] = image_inputs['pixel_values'].to(self.device)
-
         image_features = self.model.get_image_features(**image_inputs)
 
-        text_tags_lists = input_data["tags"]
-        text_features_list = []
-        for text_tag_list in text_tags_lists:
+        result_probabilities = {}
+        for tag_group, text_tag_list in tags.items():
+            result_probabilities[tag_group] = {}
+
             self.processor(text=text_tag_list, images=image, return_tensors="pt", padding=True)
             text_inputs = self.tokenizer(text_tag_list, padding=True, return_tensors="pt")
             self._input_to_device(text_inputs)
 
             text_features = self.model.get_text_features(**text_inputs)
-            text_features_list.append(text_features)
-        return image_features, text_features_list
+            logits = image_features @ text_features.T
+            probabilities = self.soft_max_function(logits)
+            probabilities = probabilities.flatten().tolist()
+
+            for tag, prob in zip(text_tag_list, probabilities):
+                r_prob = round(prob, 3)
+                result_probabilities[tag_group][tag] = r_prob
+
+        return result_probabilities
 
     def _input_to_device(self, input_data:dict):
         """
