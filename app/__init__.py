0a13735d0fead2c5974539ddc609a6398c032786
TImofey Starzhevsky <yarik.mist@gmail.com>
2024-05-12 21:30:26 +0300

    [+] Added root path to api.

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,18 @@
 from fastapi import Depends, FastAPI
 from app.internal.routes import __routes__
 from app.pkg.middlewares.x_auth_token import x_auth_token
+from app.pkg.settings import settings
 
 def create_app():
     """All settings for the application."""
-    app = FastAPI(dependencies=[Depends(x_auth_token)])
+    if settings.IS_PROD:
+        configs = settings.API.CONFIGS
+    else:
+        configs = {}
+
+    app = FastAPI(
+        **configs,
+        dependencies=[Depends(x_auth_token)],
+    )
     __routes__.register_routes(app=app)
     return app
